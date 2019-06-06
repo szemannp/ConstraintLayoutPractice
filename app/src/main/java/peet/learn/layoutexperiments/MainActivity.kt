c50d2267870108de40fd89d4bc76ef990f83342f
Szemann Peter <pete@rollet.hu>
2019-06-06 18:20:30 -0400

    constraint layout practices

---
+++ app/src/main/java/peet/learn/layoutexperiments/MainActivity.kt
@@ -0,0 +1,13 @@
+package peet.learn.layoutexperiments
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+        setContentView(R.layout.note_layout)
+    }
+}
